6dbe67014fea52af262907f2e782071d65b8d72c
hsc396612325 <heshucheng@xiyou3g.com>
2017-09-25 19:52:05 +0800

    第一次测试

--- app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
+++ app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ///tfyghj
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
