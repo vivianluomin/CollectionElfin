ab45234be53368ef87f090dd5e6bc02b928937d6
vivianluomin <273104241@qq.com>
2017-09-26 17:19:10 +0800

    建立抽屉布局

--- app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
+++ app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    ///tfyghj
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
