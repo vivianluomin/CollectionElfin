9145bc52bad920f28934c1fbf7df8ca4d1d31fe9
jiangtingting <18792898826@163.com>
2017-09-28 09:56:55 +0800

    ReadActivity

--- app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
+++ app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.asus1.collectionelfin;
 
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.asus1.collectionelfin.activities.ReadActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //借我一个按钮
+        ImageView button = (ImageView) findViewById(R.id.iv_openDrawer);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ReadActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
