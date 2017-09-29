3cd6945824ccddd35b2da919eeb4bfcf463d48d7
sakurakid <610444099@qq.com>
2017-09-29 21:59:52 +0800

    界面还是要改的，，，，

--- app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
+++ app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.asus1.collectionelfin;
 
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.example.asus1.collectionelfin.Utills.Floating;
 import com.example.asus1.collectionelfin.activities.ReadActivity;
 
 
@@ -26,7 +29,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //悬浮按钮的
+        FloatingActionButton fag = (FloatingActionButton) findViewById(R.id.but_fab);
+        fag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Floating.class);
+                startActivity(intent);
+            }
+        });
+
+        //上面的menu的
+        ImageView meanu = (ImageView) findViewById(R.id.iv_switch);
+        meanu.setOnClickListener(new ImageButton.OnClickListener() {
 
 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                //onKeyDown(KeyEvent.KEYCODE_MENU, null);
+                openOptionsMenu();
+            }
+        });
     }
 }
