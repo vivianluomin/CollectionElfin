349afc79bd5e79522300065da23c24d81e4026d9
sakurakid <610444099@qq.com>
2017-09-29 10:19:38 +0800

    Merge branch 'master' of D:\CollctionElfin with conflicts.

--- app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
+++ app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.asus1.collectionelfin;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-import com.example.asus1.collectionelfin.activities.ReadActivity;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.but_fab);
 
+        fab.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.but_fab:
+                Intent intent = new Intent(MainActivity.this,FloatingButtonMenu.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
