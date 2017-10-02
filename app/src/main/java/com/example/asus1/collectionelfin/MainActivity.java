da898a4e5a651904b2c779eb39a0a5d9c9a95c78
vivianluomin <273104241@qq.com>
2017-10-02 17:14:28 +0800

    删除某个傻逼的东西，重写写

--- app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
+++ app/src/main/java/com/example/asus1/collectionelfin/MainActivity.java
@@ -1,32 +1,103 @@
 package com.example.asus1.collectionelfin;
 
 
-import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-
-import com.example.asus1.collectionelfin.activities.ReadActivity;
-
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** 抽屉视图 */
+    private DrawerLayout mDrawerLayout;
+
+    /** 侧滑菜单视图 */
+    private NavigationView mMenuNv;
+
+    /** 打开侧滑菜单 */
+    private TextView mOpenMenuTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //借我一个按钮
-        ImageView button = (ImageView) findViewById(R.id.iv_openDrawer);
-        button.setOnClickListener(new View.OnClickListener() {
+        //初始化界面
+        initUI();
+        //初始化监听
+        initListener();
+    }
+
+    /**
+     * 初始化界面
+     */
+    private void initUI() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mMenuNv = (NavigationView) findViewById(R.id.nv_layout);
+        mOpenMenuTv = (TextView) findViewById(R.id.tv_open);
+    }
+
+    /**
+     * 初始化监听
+     */
+    private void initListener() {
+
+        mOpenMenuTv.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ReadActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                // 打开侧滑菜单
+                mDrawerLayout.openDrawer(GravityCompat.START);
             }
         });
 
+        // 设置侧滑菜单点击事件监听
+        mMenuNv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
 
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                selectItem(item.getItemId());
+                // 关闭侧滑菜单
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
+
+    /**
+     * 响应item点击事件
+     * @param itemid
+     */
+    private void selectItem(int itemid) {
+        switch (itemid) {
+            case R.id.menu_mon:
+                Toast.makeText(MainActivity.this, "点击Mon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_tues:
+                Toast.makeText(MainActivity.this, "点击Tues", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_wed:
+                Toast.makeText(MainActivity.this, "点击Wed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_thurs:
+                Toast.makeText(MainActivity.this, "点击Thurs", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_fri:
+                Toast.makeText(MainActivity.this, "点击Fri", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_sat:
+                Toast.makeText(MainActivity.this, "点击 Sat", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_sun:
+                Toast.makeText(MainActivity.this, "点击 Sun", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+
 }
--- app/src/main/java/com/example/asus1/collectionelfin/activities/ReadActivity.java
+++ app/src/main/java/com/example/asus1/collectionelfin/activities/ReadActivity.java
@@ -38,69 +38,11 @@ public class ReadActivity extends AppCompatActivity {
     }
 
     private void setUpViews() {
-        mToolbar = (Toolbar) findViewById(R.id.read_page_tool_bar);
-        setSupportActionBar(mToolbar);
 
-        mBarTitle = (TextView) findViewById(R.id.read_page_title);
-//        setTitle(mCollectionTitle);
-        mBarTitle.setText("大概的这样吗？");
-
-        mBack = (ImageView) findViewById(R.id.read_page_back_button);
-        mBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-        mWebView = (WebView) findViewById(R.id.wv_showArtical);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new WebViewClient());
-//        mWebView.loadUrl(mCollectionUrl);
-
-        mWebView.loadUrl("https://github.com/nyakokishi/WeChatRadioBar");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.read_page_menu, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.read_page_menu_note:
-                Toast.makeText(this, "跳转添加笔记页面", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.read_page_menu_remove:
-                AlertDialog.Builder dialog = new AlertDialog.Builder(ReadActivity.this);
-                dialog.setTitle("删除");
-                dialog.setMessage("删除将清空对应笔记，确定删除？");
-                dialog.setCancelable(true);
-                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //删除收藏记录。。。
-                        finish();
-                    }
-                });
-                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                    }
-                });
-                dialog.show();
-                break;
-            case R.id.read_page_menu_share:
-                Toast.makeText(this, "想弹出分享框", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.read_page_menu_link:
-                //返回链接到粘贴板
-                Toast.makeText(this, "复制链接成功", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        return true;
-    }
+
+
 
 }
