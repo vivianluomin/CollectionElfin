d6813a205c6cd884955e2b828edb027d1633f0d6
vivianluomin <273104241@qq.com>
2017-10-03 19:58:17 +0800

    加几个fragment

---
+++ app/src/main/java/com/example/asus1/collectionelfin/fragments/CollectionFragment.java
@@ -0,0 +1,10 @@
+package com.example.asus1.collectionelfin.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by asus1 on 2017/10/3.
+ */
+
+public class CollectionFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/asus1/collectionelfin/fragments/NoteFragment.java
@@ -0,0 +1,8 @@
+package com.example.asus1.collectionelfin.fragments;
+
+/**
+ * Created by asus1 on 2017/10/3.
+ */
+
+public class NoteFragment  {
+}
