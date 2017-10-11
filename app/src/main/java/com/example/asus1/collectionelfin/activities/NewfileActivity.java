2717132a00ebc3f733797bccbc404081ed5a0298
sakurakid <610444099@qq.com>
2017-10-11 22:15:04 +0800

    把新建收藏夹和笔记的界面改了下，，逻辑代码还在学习存储

--- app/src/main/java/com/example/asus1/collectionelfin/activities/NewfileActivity.java
+++ app/src/main/java/com/example/asus1/collectionelfin/activities/NewfileActivity.java
@@ -1,20 +1,25 @@
 package com.example.asus1.collectionelfin.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.example.asus1.collectionelfin.R;
 
 public class NewfileActivity extends  BaseActivity {
-    private ImageView mBack;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newfile);
-        mBack = (ImageView)findViewById(R.id.iv_newfile_back_button);
-        mBack.setOnClickListener(new View.OnClickListener() {
+        init();
+
+    }
+    private void init(){
+        mToolbar = (Toolbar) findViewById(R.id.newfile_tool_bar);
+        mToolbar.setNavigationIcon(R.mipmap.ic_back);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
