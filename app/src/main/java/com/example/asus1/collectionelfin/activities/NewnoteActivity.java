1e371b503795f8f7eee3b2d84b52f03638bffc46
sakurakid <610444099@qq.com>
2017-10-23 22:00:55 +0800

    耶耶耶，自定义了输入笔记那块，可以加图片，和文字，混合输入

--- app/src/main/java/com/example/asus1/collectionelfin/activities/NewnoteActivity.java
+++ app/src/main/java/com/example/asus1/collectionelfin/activities/NewnoteActivity.java
@@ -1,85 +1,139 @@
 package com.example.asus1.collectionelfin.activities;
 
-import android.app.ActionBar;
+import android.Manifest;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 
+import com.example.asus1.collectionelfin.PictureAndTextEditorView.PictureAndTextEditorView;
 import com.example.asus1.collectionelfin.R;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class NewnoteActivity extends BaseActivity {
-
-    private File mFile;
-    private EditText mEdit;
-
+    private PictureAndTextEditorView mEditText;
+    private FloatingActionButton fab;
+    private Uri imageUri;
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newnote);
 
-        Intent intent = getIntent();
-        String url = intent.getStringExtra("file");
-        if(url!=null&&!url.equals("")){
-            mFile =  new File(url);
-        }
+        mEditText = (PictureAndTextEditorView) findViewById(R.id.edit_PAE);
+        fab = (FloatingActionButton) findViewById(R.id.but_add_picture);
+        mToolbar = (Toolbar) findViewById(R.id.tool_newnote);
+        mToolbar.setNavigationIcon(R.mipmap.ic_back);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(NewnoteActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(NewnoteActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                }else{
+                    selectPicFromLocal();
+                }
 
-        Toolbar toolBar=(Toolbar)findViewById(R.id.tool_newnote);
-        CollapsingToolbarLayout collapsing=(CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar_newnote);
-        setSupportActionBar(toolBar);
-        android.support.v7.app.ActionBar actionBar=getSupportActionBar();
-        mEdit = (EditText)findViewById(R.id.et_edittext);
-        if(actionBar!=null){
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setHomeAsUpIndicator(R.mipmap.ic_back);
-        }
-        collapsing.setTitle("我的笔记");
-        if(mFile !=null){
-            setData();
-        }
+            }
+        });
+
+
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.i("EditActivity", mEditText.getmContentList().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
     }
 
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case 888:
+                    if (data != null) {
 
-    private void setData(){
-        try {
-            BufferedReader reader = new
-                    BufferedReader(new InputStreamReader(new FileInputStream(mFile)));
+                         handleImageOnKitKat(data);
+                    }
+                default:
+                    break;
 
-            String s;
-            StringBuilder builder = new StringBuilder();
-            while((s = reader.readLine())!=null){
-
-                builder.append(s);
             }
-
-            mEdit.setText(builder.toString());
-
-        }catch (IOException e){
-
         }
+    }
+    private String getImageAbsolutePath(Uri uri,String selection){
+        String path = null;
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        if(cursor!=null){
+            if(cursor.moveToFirst()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+    private void selectPicFromLocal() {
+        Intent intent1 = new Intent("android.intent.action.GET_CONTENT");
+        intent1.setType("image/*");
+        startActivityForResult(intent1,888);
+    }
+    private void handleImageOnKitKat(Intent data){
+        String imagePath = null;
+        Uri uri = data.getData();
+        if(DocumentsContract.isDocumentUri(this,uri)){
+            String docId = DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImageAbsolutePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if("com.android.provider.dowmloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),
+                        Long.valueOf(docId));
+                imagePath = getImageAbsolutePath(contentUri,null);
+            }
+        }else if("content".equalsIgnoreCase(uri.getScheme())){
+            imagePath = getImageAbsolutePath(uri,null);
+        }else if("file".equalsIgnoreCase(uri.getScheme())){
+            imagePath = uri.getPath();
+        }
+        mEditText.insertBitmap(imagePath);
+    }
+
+
+
+
+
 
     }
 
-}
+
 
